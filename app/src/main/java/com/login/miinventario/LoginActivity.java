a0bf7683a0c3df2f6742aea4fe6dde85163ef824
Ricardo <s20120031@itsm-tlapa.edu.mx>
2025-01-10 00:20:10 -0600

    funcionamiento del LoginActivity

--- app/src/main/java/com/login/miinventario/LoginActivity.java
+++ app/src/main/java/com/login/miinventario/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.login.miinventario;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,29 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+
+        EditText etUsuario = findViewById(R.id.etUsuario);
+        EditText etPassword = findViewById(R.id.etPassword);
+        Button btnIngresar = findViewById(R.id.btnLogin);
+
+        btnIngresar.setOnClickListener(v -> {
+            String usuario = etUsuario.getText().toString();
+            String contrasenia = etPassword.getText().toString().trim();
+            if (usuario.isEmpty() || contrasenia.isEmpty()) {
+                Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+            }
+            else if (usuario.equals("Admin") && contrasenia.equals("4dm1nRmb")) { // Lógica de inicio de sesión
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+            else if (usuario.equals("1") && contrasenia.equals("1")) { // Lógica de inicio de sesión
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+            else {
+                Toast.makeText(this, "Error de igreso", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainlogin), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
