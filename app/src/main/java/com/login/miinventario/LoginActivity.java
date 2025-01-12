7e1c8800505ad187147e0491e7057ccaaf627599
Ricardo <s20120031@itsm-tlapa.edu.mx>
2025-01-12 00:53:50 -0600

    Se corrigio un error

--- app/src/main/java/com/login/miinventario/LoginActivity.java
+++ app/src/main/java/com/login/miinventario/LoginActivity.java
@@ -44,10 +44,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainlogin), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
